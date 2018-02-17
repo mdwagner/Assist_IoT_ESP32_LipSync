c338b7fe9f68be59991cb40e0f2787768bc2e601
Michael Wagner <mdwranger@gmail.com>
2018-02-17 11:56:29 -0500

    remove useless html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import Relay from './relay.html';
-import Example4 from './example4.html';
-import Example5 from './example5.html';
-import RelayWip from './relaywip.html';
-import Slider from './slider.html';
 
 console.log('Hello world');
