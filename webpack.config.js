519529af01067fc32657ae9076c2fa0be61b2b90
Michael Wagner <mdwranger@gmail.com>
2018-02-17 11:29:04 -0500

    wip comments

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,6 @@ const PATHS = {
 };
 
 module.exports = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
   entry: {
     app: PATHS.src
   },
