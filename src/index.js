018fe11143b94f3fb2009bd7d1eda5716692d680
Michael Wagner <mdwranger@gmail.com>
2018-02-17 14:58:34 -0500

    fixed js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,46 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: 0
+    };
+    // var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);
+    // var connection = new WebSocket('ws://192.168.4.1:81/', ['arduino']);
+    this.connection = new WebSocket('ws://0.0.0.0:5001');
+  }
+
+  touchStart = (id) => {
+    // this.connection.send(`${id}`);
+    console.log(`${id}`);
+  }
+
+  inputChange = (e) => {
+    if (e) {
+      e.persist();
+      this.setState(prevState => Object.assign({}, prevState, { inputValue: e.target.value }),
+        this.touchStart(e.target.value));
+    }
+  }
+
   render() {
     return (
       <div>
-        <div id="centerapp">
-          <b>LipSync Omni Contoller</b>
-          <div className="container containerPlacement" >
-            <div id="g1" className="gauge"></div>
-            <button id="I" className="button" type="button">Mouse</button>
-            <button id="M" className="button" type="button">JoyStick</button>
-            <button id="L" className="button" type="button">TV Remote</button>
-            <button id="R" className="button" type="button">Playstation</button>
-            <button id="T" className="button" type="button">Test Comms</button>
+        <div id={"centerapp"}>
+          <h1>LipSync Omni Contoller</h1>
+          <div className={"container containerPlacement"}>
+            <div id={"g1"} className={"gauge"}></div>
+            <button id={"I"} className={"button"} type={"button"} onClick={() => this.touchStart('I')}>Mouse</button>
+            <button id={"M"} className={"button"} type={"button"} onClick={() => this.touchStart('M')}>JoyStick</button>
+            <button id={"L"} className={"button"} type={"button"} onClick={() => this.touchStart('L')}>TV Remote</button>
+            <button id={"R"} className={"button"} type={"button"} onClick={() => this.touchStart('R')}>Playstation</button>
+            <button id={"T"} className={"button"} type={"button"} onClick={() => this.touchStart('T')}>Test Comms</button>
           </div>
-          <div className="speedControl">
-            <label htmlFor="points">SPEED CONTROL</label>
-            <input type="range" id="speed_id" value="0" min="0" max="1023" onChange="connection.send(this.value)" />
+          <div className={"speedControl"}>
+            <label htmlFor={"points"}>SPEED CONTROL</label>
+            <input type={"range"} id={"speed_id"} value={this.state.inputValue} min={"0"} max={"1023"} onChange={this.inputChange} />
           </div>
         </div>
       </div>
